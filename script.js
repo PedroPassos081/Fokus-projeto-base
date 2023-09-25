c387440b8afbb71abf02081eeb1695a050fdcb8f
PedroPassos081 <pedro.passos081@gmail.com>
2023-09-25 09:48:21 -0300

    fazendo interagir o botao de pausa e play

--- script.js
+++ script.js
@@ -1,80 +1,105 @@
-const html = document.querySelector('html')
-const focoBt = document.querySelector('.app__card-button--foco')
-const curtoBt = document.querySelector('.app__card-button--curto')
-const longoBt = document.querySelector('.app__card-button--longo')
-const banner = document.querySelector('.app__image')
-const titulo = document.querySelector('.app__title')
-const botoes = document.querySelectorAll('.app__card-button')
-const startPausebt = document.querySelector('#start-pause')
+const html = document.querySelector("html");
+const focoBt = document.querySelector(".app__card-button--foco");
+const curtoBt = document.querySelector(".app__card-button--curto");
+const longoBt = document.querySelector(".app__card-button--longo");
+const banner = document.querySelector(".app__image");
+const titulo = document.querySelector(".app__title");
+const botoes = document.querySelectorAll(".app__card-button");
+const startPausebt = document.querySelector("#start-pause");
+const iniciarOuPausarBt = document.querySelector("#start-pause span");
+const iconePause = document.querySelector(".app__card-primary-butto-icon");
 
-const musicaFoco = document.querySelector('#alternar-musica')
-const musica = new Audio('./sons/luna-rise-part-one.mp3')
-musica.loop = true
+const musicaFoco = document.querySelector("#alternar-musica");
+const musica = new Audio("./sons/luna-rise-part-one.mp3");
+musica.loop = true;
 
-let tempoDecorridoEmSegundos = 5
-let intervaloId = null
+const audioPlay = new Audio("/sons/play.wav");
+const audioPausa = new Audio("/sons/pause.mp3");
+const audioTempoFinalizado = new Audio("./sons/beep.mp3");
 
-musicaFoco.addEventListener('change', () => {
-    if (musica.paused) {
-        musica.play()
-    } else { 
-        musica.pause()
-    }
-})
+let tempoDecorridoEmSegundos = 5;
+let intervaloId = null;
 
-focoBt.addEventListener('click', () => {
-  alterarContexto('foco')
-  focoBt.classList.add('active')
-})
+musicaFoco.addEventListener("change", () => {
+  if (musica.paused) {
+    musica.play();
+  } else {
+    musica.pause();
+  }
+});
 
-curtoBt.addEventListener('click', () => {
-   alterarContexto('descanso-curto')
-   curtoBt.classList.add('active')
-})
+focoBt.addEventListener("click", () => {
+  alterarContexto("foco");
+  focoBt.classList.add("active");
+});
 
-longoBt.addEventListener('click', () => {
-  alterarContexto('descanso-longo')
-  longoBt.classList.add('active')
-})
+curtoBt.addEventListener("click", () => {
+  alterarContexto("descanso-curto");
+  curtoBt.classList.add("active");
+});
 
-function alterarContexto (contexto) {
-    botoes.forEach((contexto) => {
-        contexto.classList.remove('active')
-    })
-    html.setAttribute('data-contexto', contexto )
-    banner.setAttribute('src', `./imagens/${contexto}.png`)
-    switch (contexto) {
-        case "foco":
-            titulo.innerHTML = `
+longoBt.addEventListener("click", () => {
+  alterarContexto("descanso-longo");
+  longoBt.classList.add("active");
+});
+
+function alterarContexto(contexto) {
+  botoes.forEach((contexto) => {
+    contexto.classList.remove("active");
+  });
+  html.setAttribute("data-contexto", contexto);
+  banner.setAttribute("src", `./imagens/${contexto}.png`);
+  switch (contexto) {
+    case "foco":
+      titulo.innerHTML = `
             Otimize sua produtividade,<br>
                 <strong class="app__title-strong">mergulhe no que importa.</strong>
-            `
-            break;
-        case 'descanso-curto':
-            titulo.innerHTML = `
+            `;
+      break;
+    case "descanso-curto":
+      titulo.innerHTML = `
             Que tal dar uma respirada? <strong class="app__title-strong"> Faça uma pausa </strong>
-            `    
-            break
-        case 'descanso-longo': 
-             titulo.innerHTML = `
+            `;
+      break;
+    case "descanso-longo":
+      titulo.innerHTML = `
              Hora de voltar a superfície. <strong class="app__title-strong"> Faça uma pausa longa </strong>
-             `
-    
-        default:
-            break;
-    }
+             `;
+
+    default:
+      break;
+  }
 }
 
-const  contagemRegressiva = () => {
-    iniciar()
-    tempoDecorridoEmSegundos -=1
+const contagemRegressiva = () => {
+  if (tempoDecorridoEmSegundos <= 0) {
+    audioTempoFinalizado.play();
+    alert("Tempo finalizado");
+    zerar();
+    return;
+  }
+  tempoDecorridoEmSegundos -= 1;
+  console.log("Tempo: " + tempoDecorridoEmSegundos);
+  console.log("Id: " + intervaloId);
+};
 
+startPausebt.addEventListener("click", iniciarOuPausar);
+
+function iniciarOuPausar() {
+  if (intervaloId) {
+    audioPausa.play();
+    zerar();
+    return;
+  }
+  audioPlay.play();
+  intervaloId = setInterval(contagemRegressiva, 1000);
+  iniciarOuPausarBt.textContent = "pausar";
+  iconePause.setAttribute("src", `./imagens/pause.png`);
 }
 
-startPausebt.addEventListener('click', contagemRegressiva)
-
-function iniciar() {
-    intervaloId.setInterval((contagemRegressiva) => {
-        
-    }, interval);
+function zerar() {
+  clearInterval(intervaloId);
+  iniciarOuPausarBt.textContent = "começar";
+  iconePause.setAttribute("src", `./imagens/play_arrow.png`);
+  intervaloId = null;
 }
