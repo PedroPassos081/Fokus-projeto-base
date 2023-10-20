d61737d204913f76587c40717a97788a65cf3fdf
Pedro Passos <pedro.passos081@gmail.com>
2023-10-20 09:34:07 -0300

    Adicionando novas funções ao projeto

--- script-crud.js
+++ script-crud.js
@@ -1,15 +1,15 @@
 const taskListContainer = document.querySelector('.app__section-task-list')
 
-let tasks = [
-    {
-        descricao: 'Tarefa Concluída',
-        concluida: true
-    },
-    {
-        descricao: 'Tarefa pendente',
-        concluida: false   
-    }
-]
+const formTask = document.querySelector('.app__form-add-task')
+const toggleFormTaskBtn = document.querySelector('.app__button--add-task')
+const formLabel = document.querySelector('.app__form-label')
+
+const textarea = document.querySelector('.app__form-textarea')
+
+const cancelFormTaskBtn = document.querySelector('.app__form-footer__button--cancel')
+const btnCancelar = document.querySelector('.app__form-footer__button--cancel')
+
+let tasks = []
 
 // Recebe um caminho SVG
 const taskIconSvg = `<svg class="app_section-task-icon-status" width="24" height="24" viewBox="0 0 24 24"
@@ -20,6 +20,11 @@ fill="none" xmlns="http://www.w3.org/2000/svg">
     d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L19 16.17192"
     fill="#01080E" />
 </svg>`
+
+const limparForm = () => {
+    textarea.value = ''
+    formTask.classList.add('hidden')
+}
 
 // Função que cria tarefa e joga no HTML
 function createTask(tarefa) {
@@ -40,8 +45,37 @@ function createTask(tarefa) {
     return li
 }
 
-// Serve para que apareça a função na tela
+// Faz com que apareça a tarefa na tela
 tasks.forEach(task => {
     const taskItem = createTask(task)
     taskListContainer.appendChild(taskItem)
 })
+
+// função para abrir uma tarefa nova 
+toggleFormTaskBtn.addEventListener('click', () => {
+    formLabel.textContent = 'Adicionando tarefa'
+    formTask.classList.toggle('hidden')
+})
+
+// função para cancelar a tarefa 
+cancelFormTaskBtn.addEventListener('click', () => {
+    formTask.classList.add('hidden')
+
+    limparForm()
+})
+
+// função para adicionar tarefa 
+formTask.addEventListener('submit', (evento) => {
+    evento.preventDefault()
+    const task = {
+        descricao: textarea.value,
+        concluida: false
+    }
+    tasks.push(task)
+    const taskItem = createTask(task)
+    taskListContainer.appendChild(taskItem)
+
+    limparForm()
+})
+
+localStorage.setItem('quantidade', 10) 
