62d31f5b2433061ed0b6accbc05c5dd7aa54864b
Pedro Passos <pedro.passos081@gmail.com>
2023-10-23 16:40:48 -0300

    Adicionando a função de editar as tarefas

--- script-crud.js
+++ script-crud.js
@@ -16,17 +16,21 @@ const localStorageTasks = localStorage.getItem('tasks')
 let tasks = localStorageTasks ? JSON.parse(localStorageTasks) : []
 
 // Recebe um caminho SVG
-const taskIconSvg = `<svg class="app_section-task-icon-status" width="24" height="24" viewBox="0 0 24 24"
-fill="none" xmlns="http://www.w3.org/2000/svg">
-<circle cx="12" cy="12" r="12" 
-    fill="#FFF" />
-<path
-    d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L19 16.17192"
-    fill="#01080E" />
-</svg>`
+const taskIconSvg = `
+<svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24"
+    fill="none" xmlns="http://www.w3.org/2000/svg">
+    <circle cx="12" cy="12" r="12" fill="#FFF" />
+    <path
+        d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z"
+        fill="#01080E" />
+</svg>
+`
 
 let tarefaSelecionada = null
 let itemTarefaSelecionada = null
+
+let tarefaEmEdicao = null
+let paragraphEmEdicao = null
 
 const selecionaTarefa = (tarefa, elemento) => {
 
@@ -48,8 +52,24 @@ const selecionaTarefa = (tarefa, elemento) => {
     }
 
 const limparForm = () => {
+    tarefaEmEdicao = null
+    paragraphEmEdicao = null
     textarea.value = ''
     formTask.classList.add('hidden')
+}
+
+// Função para editar as tarefas
+const selecionaTarefaParaEditar = (tarefa, elemento) => {
+ if(tarefaEmEdicao == tarefa) {
+    limparForm()
+    return
+ }
+
+ formLabel.textContent='Editando tarefa'
+ tarefaEmEdicao = tarefa
+ paragraphEmEdicao = elemento
+ textarea.value = tarefa.descricao
+ formTask.classList.remove('hidden')
 }
 
 // Função que cria tarefa e joga no HTML
@@ -66,6 +86,17 @@ function createTask(tarefa) {
     paragraph.textContent = tarefa.descricao
 
     const button = document.createElement('button')
+
+    button.classList.add('app_button-edit')
+    const editIcon = document.createElement('img')
+    editIcon.setAttribute('src', '/imagens/edit.png')
+    
+    button.appendChild(editIcon)
+
+    button.addEventListener('click', (event) => {
+        event.stopPropagation()
+        selecionaTarefaParaEditar(tarefa, paragraph)
+    })
 
     li.onclick = () => {
         selecionaTarefa(tarefa, li)
@@ -84,6 +115,7 @@ function createTask(tarefa) {
 
     li.appendChild(svgIcon)
     li.appendChild(paragraph)
+    li.appendChild(button)
 
     return li
 }
@@ -112,9 +144,14 @@ cancelFormTaskBtn.addEventListener('click', () => {
     limparForm()
 })
 
-// função para adicionar tarefa 
+// função para adicionar tarefa e editar tarefa
 formTask.addEventListener('submit', (evento) => {
     evento.preventDefault()
+    
+    if(tarefaEmEdicao) {
+        tarefaEmEdicao.descricao = textarea.value
+        paragraphEmEdicao.textContent = textarea.value
+    } else {
     const task = {
         descricao: textarea.value,
         concluida: false
@@ -122,7 +159,7 @@ formTask.addEventListener('submit', (evento) => {
     tasks.push(task)
     const taskItem = createTask(task)
     taskListContainer.appendChild(taskItem)
-    
+    }
     updateLocalStorage()
     limparForm()
 })
